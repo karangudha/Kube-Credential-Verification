fc1bcb06e25b5610e0a0e7dc8e453afbbcb74fa5
Karan <karankgudha@gmail.com>
2025-10-08 16:31:18 +0530

    change mongodb uri to create databasea

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("mongodb connected successfully");
     }
     catch (error) {
