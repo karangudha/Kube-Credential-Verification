975d1ce626353901db988f972f361f76d687ae07
Karan <karankgudha@gmail.com>
2025-10-08 23:21:19 +0530

    add error handling

--- backend/src/verification.js
+++ backend/src/verification.js
@@ -3,32 +3,34 @@ const verification = async(req, res) => {
     const username = req.query.username;
     try {
         if(!username) 
-        {
-            console.log("Username not found!!")
-            throw new Error("credentials required");
-        }
+            res.status(400)
+                .json({
+                    status: false,
+                    message: "credentials required",
+                })
         
-        const isUser = await User.findOne({
-            username: username
-        })
+        const isUser = await User.findOne({username})
     
         if(!isUser) 
-        {
-            console.log("User not found in DB");
-            throw new Error("User not found");
-        }
-        console.log("User verified successfully!!")
-        res.status(200).json({
-            success: true,
-            message: "User verified",
-            data: isUser,
-        })
+            res.status(404)
+                .json({
+                    status: false,
+                    message: "User not found",
+                })
+
+        res.status(200)
+            .json({
+                success: true,
+                message: "User verified",
+                data: isUser,
+            })
     } catch (error) {
         console.log("Error: ", error);
-        res.status(400).json({
-            success: false,
-            message: error.message,
-        })
+        res.status(500)
+            .json({
+                success: false,
+                message: error.message,
+            })
     }   
 }
 export { verification };
